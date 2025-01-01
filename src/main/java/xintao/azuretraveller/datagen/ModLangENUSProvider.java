8b545b55400f787e504712fd9aa05dd6718aa85c
xintao <xintao0312@163.com>
2025-01-01 11:19:34 +0800

    24.1231.1

---
+++ src/main/java/xintao/azuretraveller/datagen/ModLangENUSProvider.java
@@ -0,0 +1,21 @@
+package xintao.azuretraveller.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricLanguageProvider;
+import net.minecraft.registry.RegistryWrapper;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModLangENUSProvider extends FabricLanguageProvider
+{
+    protected ModLangENUSProvider(FabricDataOutput dataOutput, CompletableFuture<RegistryWrapper.WrapperLookup> registryLookup)
+    {
+        super(dataOutput, "en_us", registryLookup);
+    }
+
+    @Override
+    public void generateTranslations(RegistryWrapper.WrapperLookup registryLookup, TranslationBuilder translationBuilder)
+    {
+        
+    }
+}
