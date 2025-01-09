15e342ad4042316dbedb46ef7059de8cb7289fbf
xintao <xintao0312@163.com>
2025-01-09 21:08:03 +0800

    25.0109.1

--- src/main/java/xintao/azuretraveller/item/ModArmorMaterials.java
+++ src/main/java/xintao/azuretraveller/item/ModArmorMaterials.java
@@ -21,28 +21,28 @@ public class ModArmorMaterials
     public static final RegistryEntry<ArmorMaterial> MYTHRIL = 
             register("mythril", Util.make(new EnumMap(ArmorItem.Type.class), 
             map -> 
-            {
-                map.put(ArmorItem.Type.BOOTS, 3);
-                map.put(ArmorItem.Type.LEGGINGS, 6);
-                map.put(ArmorItem.Type.CHESTPLATE, 8);
-                map.put(ArmorItem.Type.HELMET, 3);
-                map.put(ArmorItem.Type.BODY, 11);
-            }
+                {
+                    map.put(ArmorItem.Type.BOOTS, 3);
+                    map.put(ArmorItem.Type.LEGGINGS, 6);
+                    map.put(ArmorItem.Type.CHESTPLATE, 8);
+                    map.put(ArmorItem.Type.HELMET, 3);
+                    map.put(ArmorItem.Type.BODY, 11);
+                }
             ), 
             15, SoundEvents.ITEM_ARMOR_EQUIP_NETHERITE, 3.0f, 0.1f, 
                     () -> Ingredient.ofItems(ModItems.MYTHRIL_INGOT));
 
     public static final RegistryEntry<ArmorMaterial> OBSIDIAN =
             register("obsidian", Util.make(new EnumMap(ArmorItem.Type.class),
-                            map ->
-                            {
-                                map.put(ArmorItem.Type.BOOTS, 3);
-                                map.put(ArmorItem.Type.LEGGINGS, 6);
-                                map.put(ArmorItem.Type.CHESTPLATE, 8);
-                                map.put(ArmorItem.Type.HELMET, 3);
-                                map.put(ArmorItem.Type.BODY, 11);
-                            }
-                    ),
+                            map -> 
+                {
+                    map.put(ArmorItem.Type.BOOTS, 3);
+                    map.put(ArmorItem.Type.LEGGINGS, 6);
+                    map.put(ArmorItem.Type.CHESTPLATE, 8);
+                    map.put(ArmorItem.Type.HELMET, 3);
+                    map.put(ArmorItem.Type.BODY, 11);
+                }
+            ),
                     15, SoundEvents.ITEM_ARMOR_EQUIP_NETHERITE, 1.5f, 0.0f,
                     () -> Ingredient.ofItems(ModItems.OBSIDIAN_INGOT));
     
